bacc102ecb77e1470b952c55d054a675e9750f73
Chris Johnson <csjohnson3@wsgc.com>
2020-04-04 22:21:28 -0700

    better logging

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def main():
             full_path = os.path.join(directory, filename)
             file_size = get_file_size(full_path)
             print(
-                "{} - Found '{}' and it's size is {}".format(datetime.now().strftime("%H:%M:%S"), filename, file_size),
+                "{} - Found '{}' and it's size is {}".format(datetime.now().strftime("%b %d %H:%M:%S"), filename, file_size),
                 flush=True)
             time.sleep(10)
             # loop until the file size stops growing
@@ -31,7 +31,7 @@ def main():
                 file_size = get_file_size(full_path)
                 time.sleep(10)
             print(
-                "{} moving '{}' to '{}/{}'".format(datetime.now().strftime("%H:%M:%S"), full_path, move_path, filename),
+                "{} - Moving '{}' to '{}/{}'".format(datetime.now().strftime("%b %d %H:%M:%S"), full_path, move_path, filename),
                 flush=True)
             shutil.move(full_path, "{}/{}".format(move_path, filename))
             file, extension = os.path.splitext(filename)
