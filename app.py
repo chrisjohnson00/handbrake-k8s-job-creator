4208bbc8b0ec10bb8366c962d3b16561b4aeee7c
Chris Johnson <csjohnson3@wsgc.com>
2020-04-04 22:18:30 -0700

    forcing flush

--- app.py
+++ app.py
@@ -23,14 +23,16 @@ def main():
             full_path = os.path.join(directory, filename)
             file_size = get_file_size(full_path)
             print(
-                "{} - Found '{}' and it's size is {}".format(datetime.now().strftime("%H:%M:%S"), filename, file_size))
+                "{} - Found '{}' and it's size is {}".format(datetime.now().strftime("%H:%M:%S"), filename, file_size),
+                flush=True)
             time.sleep(10)
             # loop until the file size stops growing
             while file_size != get_file_size(full_path):
                 file_size = get_file_size(full_path)
                 time.sleep(10)
             print(
-                "{} moving '{}' to '{}/{}'".format(datetime.now().strftime("%H:%M:%S"), full_path, move_path, filename))
+                "{} moving '{}' to '{}/{}'".format(datetime.now().strftime("%H:%M:%S"), full_path, move_path, filename),
+                flush=True)
             shutil.move(full_path, "{}/{}".format(move_path, filename))
             file, extension = os.path.splitext(filename)
             job_suffix = file.replace(" ", "").lower()
