0291a4ffaa346b2baafdde4a2502c0fee04e63b1
Chris Johnson <chrisjohnson00@hotmail.com>
2021-02-11 16:25:56 -0800

    fix: Job names fail if they have _ in them

--- app/utils.py
+++ app/utils.py
@@ -5,4 +5,5 @@ def cleanup_job_suffix(input_string):
     # keep only word character, removing all non-word chars
     job_suffix = re.sub(r"\W", "", input_string.lower(), flags=re.I)
     job_suffix = re.sub(r"[à-ú]|[À-Ú]", "", job_suffix, flags=re.I)
+    job_suffix = job_suffix.replace("_", "")
     return job_suffix
